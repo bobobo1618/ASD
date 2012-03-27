bc39697003fa8ca04a79734c82489acde2e18789
bobobo1618 <bobobo1618@gmail.com>
2012-03-27 23:03:32 +1100

    Added Bootstrap plugins, CodeMirror and fixed the post page.

---
+++ Static/js/editorload.coffee
@@ -0,0 +1,18 @@
+editor = 0
+
+editorConfig =  {
+    mode: 'markdown'
+    lineNumbers: true
+    theme: 'eclipse'
+}
+
+$('document').ready ()->
+    editor = CodeMirror.fromTextArea document.getElementById 'bodyarea'
+    for key, value of editorConfig
+        editor.setOption key, value
+
+switchMarkup = (markup)->
+    label = document.getElementById 'markdownlabel'
+    label.innerHTML = markup.display
+    document.getElementById('markdownfield').value = markup.name
+    editor.setOption 'mode', markup.name
--- Static/js/editorload.js
+++ Static/js/editorload.js
@@ -1,3 +1,28 @@
-$(document).ready(function() {
-    $("#bodyarea").markItUp(mySettings);
+var editor, editorConfig, switchMarkup;
+
+editor = 0;
+
+editorConfig = {
+  mode: 'markdown',
+  lineNumbers: true,
+  theme: 'eclipse'
+};
+
+$('document').ready(function() {
+  var key, value, _results;
+  editor = CodeMirror.fromTextArea(document.getElementById('bodyarea'));
+  _results = [];
+  for (key in editorConfig) {
+    value = editorConfig[key];
+    _results.push(editor.setOption(key, value));
+  }
+  return _results;
 });
+
+switchMarkup = function(markup) {
+  var label;
+  label = document.getElementById('markdownlabel');
+  label.innerHTML = markup.display;
+  document.getElementById('markdownfield').value = markup.name;
+  return editor.setOption('mode', markup.name);
+};
